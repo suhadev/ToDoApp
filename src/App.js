96c862ae648ae7515ace09b7bc4169e88b505e88
suhadev <suhadevvenkatesh@gmail.com>
2019-07-02 15:54:27 +0530

    implemented Ajax requests

--- src/App.js
+++ src/App.js
@@ -3,47 +3,84 @@ import logo from './logo.svg';
 import './App.css';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
+import axios from 'axios'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state ={
-      item:[]
+    this.state = {
+      item: [],
+      loading: true
     }
   }
-  addTask = (a,e)=>{
-    let task = {id:this.generateId(),item:a}
-    this.setState({
-      item:[...this.state.item,task]
-      
-    })
-    e.value=""
+  getData = () => {
 
+    axios.get('http://localhost:8080/todos')
+      .then((res) => {
+        console.log(res.data)
+        let tasks = res.data
+        console.log(res.data)
+        this.setState({
+          item: res.data,
+          loading: false
+        })
+
+      })
   }
-  generateId = ()=> {
-    let a=  new Date
+  addTask = (a, e) => {
+    let task = { id: this.generateId(), task: a }
+    e.value = ""
+    axios.post('http://localhost:8080/todos', task)
+      .then((res) => {
+        this.setState({
+          item: [...this.state.item, res.data]
+        })
+      })
+  }
+  generateId = () => {
+    let a = new Date
     return a.toLocaleTimeString()
   }
-  deleteTask = (a)=>{
-    let filteredArray = this.state.item.filter((i)=>{
+  deleteTask = (a) => {
+    let filteredArray = this.state.item.filter((i) => {
       console.log(i.id)
-      return i.id !== a
+      return i.id === a
     })
-    console.log(filteredArray)
-    this.setState({item:filteredArray})
+    console.log(filteredArray[0].id)
+    axios.delete(`http://localhost:8080/todos/${filteredArray[0].id}`)
+      .then(() => {
+        this.getData()
+      })
+
+    //this.setState({item:filteredArray})
   }
-  markAsComplete = (e)=>{
+  markAsComplete = (e) => {
+
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.getData()
+    }, 3000)
 
   }
   render() {
-    return (
-      <div>
-        <TodoForm addTask={this.addTask}/>
-        <TodoList tasks={this.state.item} deleteTask = {this.deleteTask} markComplete={this.markAsComplete}/>
-      </div>
+    
+      let loading = ""
+      if (this.state.loading) {
+        loading = <p>Loading...please wait!</p>
+      } else {
+        loading = <TodoList tasks={this.state.item} deleteTask={this.deleteTask} markComplete={this.markAsComplete} />
+      }
 
-    )
+      return (
+        <div>
+          <TodoForm addTask={this.addTask}/>
+          {loading}
+        </div>
+
+      )
+    }
   }
-}
+
 
 export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -13,7 +13,8 @@ class TodoList extends React.Component {
             <div>
                 <ul className="list-group">
                     {this.props.tasks.map((task) => {
-                        return <li className="list-group-item">{task.item}
+                        console.log(task)
+                        return <li className="list-group-item">{task.task}
                             <button className="btn btn-danger" style={{marginLeft:100 +'px'}} onClick={this.props.deleteTask.bind(this,task.id)}>Delete</button>
                             <button className="btn btn-success" onClick={this.props.markComplete.bind(this,task.id)}>Mark Completed</button>
                         </li>
